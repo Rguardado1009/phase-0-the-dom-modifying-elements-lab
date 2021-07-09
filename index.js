c37b7788e45b1fe42d34aa865c2dedb2f6d2cac0
Ronald Guardado <rguardado1009@gmail.com>
2021-07-09 18:48:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+var newHeader =document.createElement('h1');
+newHeader.id="victory"
+const newContent = document.createTextNode('Ronald is the champion');
+
+newHeader.appendChild(newContent);
+ 
